e720bb2043dcc90128227035ff471a8a870a3064
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2019-12-20 12:02:34 -0500

    Update PROYECTO_ARDUINO_PYTHON.py

--- CLASE5_PYTHON/PROYECTO_ARDUINO_PYTHON.py
+++ CLASE5_PYTHON/PROYECTO_ARDUINO_PYTHON.py
@@ -5,17 +5,21 @@ import serial
 arduino=serial.Serial()
 # definir atributo baudrate
 arduino.baudrate = 9600
-arduino.port = "COM12"
+arduino.port = "COM8"
 #intentar conectarse al objeto serial
 try :
     #INICIAR COMUNICACIÓN EN EL PUERTO port
     arduino.open()
-    print("CONEXIÓN CON ARDUINO")
+    print("CONEXIÓN CON ARDUINO EXITOSA")
+    
 #CODIGO QUE SE EJECUTA CUANDO SE DETECTA UN ERROR
-except:
+except serial.SerialException:
     print("SE ENCONTRO UN ERROR EN EL BLOQUE try,REVISAR POR FAVOR")
 #CODIGO QUE SE EJECUTA INDEPENDIENTEMENTE SI SE ENCONTRO UN ERROR O NO
 finally:
     arduino.close()
+    print("CERRANDO RECURSO DE MANEJO DE COMUNICACIÓN SERIAL")
+
+
 
 
