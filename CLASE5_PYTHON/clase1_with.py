f405b58cfb5e3cf81a840c4a214b0966b093b5a2
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2019-12-20 11:13:00 -0500

    Add files via upload

---
+++ CLASE5_PYTHON/clase1_with.py
@@ -0,0 +1,6 @@
+#objeto que manipula archivo
+archivo=open("archivo_clase5.txt","a")
+#escribir texto en el objeto archivo
+archivo.write("hola mundo\n adios")
+#cerrar el recurso del objeto para el manejo de archivo
+archivo.close()
