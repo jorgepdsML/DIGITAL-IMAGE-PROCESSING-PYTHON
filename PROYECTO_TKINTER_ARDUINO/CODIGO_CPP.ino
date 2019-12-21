8d8c8e98f64145012b85f3f2cb8c17676bd72ebe
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2019-12-21 11:43:12 -0500

    Update CODIGO_CPP.ino

--- PROYECTO_TKINTER_ARDUINO/CODIGO_CPP.ino
+++ PROYECTO_TKINTER_ARDUINO/CODIGO_CPP.ino
@@ -6,8 +6,8 @@
 DHT sensordht(DHTTYPE,DHTPIN);
 //DEFINIR 2 VARIABLE DEL TIPO FLOTANTE
 //TIPO NOMBRE=VALOR_INICIAL;
-float temperatura;
-float humedad;
+int temperatura;
+int humedad;
 //MENSAJE PARA TEMPERATURA
 char temp_char[6];
 char hum_char[6];
@@ -20,16 +20,16 @@ Serial.begin(9600);
 void loop()
 {
   //LEER TEMPERATURA Y HUMEDAD
- temperatura=sensordht.readTemperature();
- humedad=sensordht.readHumidity();
+ temperatura=(int)sensordht.readTemperature();
+ humedad=(int)sensordht.readHumidity();
  delay(2000);
  //si se encuentra un valor nan de temperatura , no enviarlo a la laptop
- if(isnan(temperatura)==false && isnan(humedad)==false )
-  {
-    sprintf(temp_char,"%c%.1f%c",'t',temperatura,'*');
-    sprintf(hum_char,"%c%.1f%c",'h',humedad,'-');
+if (isnan(temperatura)==false && isnan(humedad)==false)
+{
+    sprintf(temp_char,"%c%d%c",'t',20,'*');
+    sprintf(hum_char,"%c%d%c",'h',25,'-');
     //ENVIAR DATOS DE TEMPERATURA Y HUMEDAD HACIA PYTHON
 Serial.write(temp_char);
 Serial.write(hum_char);
-  }
+}
 }
