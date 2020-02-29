e66d7a6fd46e21e9a7ac5e0700b20faaac87145c
jorgepdsML <45744833+jorgepdsML@users.noreply.github.com>
2020-02-29 18:03:32 -0500

    Add files via upload

---
+++ CLASE8_PYTHON_UMAKER/support.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2
+def deteccion_face(Igray,param1=None,param2=None):
+    if param1==None or param2==None:
+        param1,param2=1.3,5
+    # crear el objeto
+    faces_detect = cv2.CascadeClassifier(
+        'C:\\Users\\pdsjo\\Desktop\\Desk_files\\CARPETA_CURSOS_PROYECTOS\\ARCHIVOS_JORGE\\CLASES_ORDENADAS\\PAPERSFORIRIS'
+        '\\haarcascade_frontalface_default.xml')
+    #realizar la detección de rostros ,
+    faces=faces_detect.detectMultiScale(Igray,param1,param2)
+    #se detecto alguna cara?
+    if faces is not ():
+        return faces
+    else:
+        return None
