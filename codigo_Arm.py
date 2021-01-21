781c8c6d583aebda6381a301cdc33ad4d09f20c5
Jorge Miranda <45744833+jorgepdsML@users.noreply.github.com>
2021-01-21 12:41:36 -0500

    Update codigo_Arm.py

--- codigo_Arm.py
+++ codigo_Arm.py
@@ -1,15 +1,18 @@
-"""IMPORTAR LOS MODULOS NECESARIOS"""
+"""
+GRUPO 1 PDS
+"""
+#INTERACTUAR CON EL PUNTERO DEL MOUSE
 import pyautogui as pd
+#libreria para comunicar python con el puerto serial (COM 3)
 import serial
+#Función exit para salir del programa
 from sys import exit
+#función sleep() retardo de tiempo
 from time import sleep
 #dimensión de la pantalla
 dimension=pd.size()
-#------DIMENSIÓN ANCHO
 W=dimension[0]
-#-----DIMENSIÓN ALTO
 H=dimension[1]
-#PROTOTIPO DE FUNCIONES
 def derecha(x,y):
     for k in range(x,x+35,5):
         pd.moveTo(k,y)
@@ -27,11 +30,11 @@ def click(x,y):
     pd.click(x=x,y=y)
     pass
 #-------------------------------COMUNICACIÓN SERIAL
-Baudios=115200
-Puerto="COM3"
+Baudios=9600#velocidad en baudios
+Puerto="COM3"#puerto serial
 #CREAR objeto para la comunicación serial
 puerto_serial = serial.Serial()
-puerto_serial.timeout=5
+puerto_serial.timeout=3
 try:
     puerto_serial.port = Puerto
     puerto_serial.baudrate = Baudios
@@ -58,7 +61,29 @@ while True:
         if puerto_serial.inWaiting()>0:
             dato=puerto_serial.read()
             dato=dato.decode()
-            print(dato,'s')
+            #leer los datos de la posición actual del cursos del mouse
+            xy = pd.position()
+            x = xy.x
+            y = xy.y
+            #preguntar uqe caracter se ha recibido
+            dato=dato.lower()
+            if dato=='l':
+                #MOVIMIENTO A LA IZQUIERDA DEL MOUSE
+                print("izquierda")
+                izquierda(x,y)
+            if dato=='r':
+                print("derecha")
+                derecha(x,y)
+            if dato=='u':
+                print("arriba")
+                arriba(x,y)
+            if dato=='d':
+                abajo(x,y)
+            if dato=='c':
+                print("click")
+                click(x,y)
+            if dato=='n':
+                break
     except:
         print("EXISTE ALGUN ERROR EN LA COMUNICACIÓN!!!!")
         for i in range(3,0,-1):
